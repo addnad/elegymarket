ef8ebffd614c4d9598afcd803d4f4d0bfce20ff8
addnad <addnad@github.com>
2026-05-24 22:41:06 +0100

    fix: switch to dRPC for CORS-enabled X Layer mainnet RPC

--- lib/web3.ts
+++ lib/web3.ts
@@ -20,7 +20,7 @@ export const xlayerMainnet = defineChain({
   name: "X Layer",
   nativeCurrency: { name: "OKB", symbol: "OKB", decimals: 18 },
   rpcUrls: {
-    default: { http: ["https://rpc.xlayer.tech"] },
+    default: { http: ["https://xlayer.drpc.org"] },
   },
   blockExplorers: {
     default: { name: "OKLink", url: "https://www.oklink.com/xlayer" },
